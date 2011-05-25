1d35b3aa40bd94b9e766e3361ef9a30dc7f704b6
Carmen Wick <CarmenWick@gmail.com>
2011-05-24 19:49:12 -0700

    Auto-indent after yanking

--- init.el
+++ init.el
@@ -268,6 +268,22 @@ is a comment, uncomment."
 	(comment-or-uncomment-line lines)))
 
 ;;
+;; Auto-indent after yanking
+;;
+(dolist (command '(yank yank-pop))
+  (eval `(defadvice ,command (after indent-region activate)
+		   (and (not current-prefix-arg)
+				(member major-mode '(emacs-lisp-mode lisp-mode
+													 clojure-mode    scheme-mode
+													 haskell-mode    ruby-mode
+													 rspec-mode      python-mode
+													 c-mode          c++-mode
+													 objc-mode       latex-mode
+													 plain-tex-mode))
+				(let ((mark-even-if-inactive transient-mark-mode))
+				  (indent-region (region-beginning) (region-end) nil))))))
+
+;;
 ;; Global keymap
 ;;
 (global-set-key (kbd "<kp-delete>") 'delete-char)
