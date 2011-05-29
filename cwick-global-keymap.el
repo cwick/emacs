e86dd3633aaaca31c61b0411df9f05425faedcc7
Carmen Wick <CarmenWick@gmail.com>
2011-05-29 05:22:18 -0700

    Cleanup and rgrep

--- cwick-global-keymap.el
+++ cwick-global-keymap.el
@@ -1,9 +1,10 @@
 ;;;; Global keymap
 
 ;;; Set up some Windows-like keybindings
-(global-set-key (kbd "C-v") 'yank)	;Windows-style ctrl+v paste
-(global-set-key (kbd "M-v") 'yank-pop)	;Cycle through kill ring
-(global-set-key (kbd "C-z") 'undo)	;Windows-style ctrl+z undo
+(global-set-key (kbd "C-v") 'yank)		; Windows-style ctrl+v paste
+(global-set-key (kbd "M-v") 'yank-pop)	; Cycle through kill ring
+(global-set-key (kbd "C-z") 'undo)		; Windows-style ctrl+z undo
+
 ;; Make home and end behave like they do on Windows
 (global-set-key (kbd "<home>") 'beginning-of-line)
 (global-set-key (kbd "<end>") 'end-of-line)
@@ -13,17 +14,21 @@
 ;; TODO: this causes backspace to stop working in incremental search
 ;; (global-set-key (kbd "<kp-delete>") 'delete-char)
 ;; (global-set-key (kbd "<backspace>") 'backward-delete-char-untabify)
+
 ;; Skip around quickly using the arrow keys
 (global-set-key (kbd "<C-left>") 'backward-word)
 (global-set-key (kbd "<C-up>") 'backward-paragraph)
 (global-set-key (kbd "<C-right>") 'forward-word)
 (global-set-key (kbd "<C-down>") 'forward-paragraph)
+
 ;; Delete more stuff with Ctrl modifier
 (global-set-key (kbd "<C-delete>") 'kill-word)
 (global-set-key (kbd "<C-kp-delete>") 'kill-word)
 (global-set-key (kbd "<C-backspace>") 'backward-kill-word)
+
 ;; Use the buffer menu (same as buffer list but displays in current buffer)
 (global-set-key (kbd "C-x C-b") 'buffer-menu)
+
 ;; Replace default text expand with hippie expand
 (global-set-key (kbd "M-/") 'hippie-expand)
 
@@ -39,6 +44,9 @@
 
 ;; Cycle through active frames
 (global-set-key (kbd "M-`") 'other-frame)
+
+;; Quick search through some files
+(global-set-key (kbd "C-S-f") 'rgrep)
 
 ;;;
 ;;; Function keys
