7bf30fcef24475b3d722523f2183f25a6de69c9d
Carmen Wick <CarmenWick@gmail.com>
2011-05-29 16:45:38 -0700

    Adding missing files

---
+++ cwick-speedbar.el
@@ -0,0 +1,28 @@
+;;;; Speedbar customizations
+
+(require 'speedbar)
+
+;; Skip over nodes with C-up / C-down
+(define-key speedbar-file-key-map (kbd "<C-up>") 'speedbar-restricted-prev)
+(define-key speedbar-file-key-map (kbd "<C-down>") 'speedbar-restricted-next)
+
+;; Show all files
+(setq speedbar-show-unknown-files t)
+
+;; Hooks
+(add-hook 'speedbar-mode-hook 
+		  (lambda ()
+			(setq case-fold-search t)	; Enable case-insensitive search
+			(speedbar-disable-update)))
+
+; Speedbar parameters. I like the speedbar to be a little wider
+;; (setq speedbar-frame-parameters
+;; 	  (quote
+;; 	   (
+;; 		(minibuffer)
+;; 		(width . 30)
+;; 		(border-width . 0)
+;; 		(menu-bar-lines . 0)
+;; 		(tool-bar-lines . 0)
+;; 		(unsplittable . t)
+;; 		(left-fringe . 0))))
