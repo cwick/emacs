5f78f730e338d6a75407a2c8671738fc8a75c776
Carmen Wick <CarmenWick@gmail.com>
2011-06-02 21:07:50 -0700

    Find tweaks

--- cwick-find.el
+++ cwick-find.el
@@ -5,5 +5,11 @@
    (list (read-regexp "Find in open buffers" (car regexp-history))))
   (occur-1 regexp nil (buffer-list)))
 
+;; Use shift-tab to go to previous match
 (define-key grep-mode-map (kbd "<S-tab>") 'compilation-previous-error)
 
+;; Pass the -I flag to grep, which tells it to ignore binary files.
+;; This lets us significantly reduce the length of the ignore files list.
+(setq grep-find-template "find . <X> <F> -type f -print0 | \"xargs\" -0 -e grep -I <C> -nH -e <R>")
+;; Reduce the length of the ignored files list to reduce clutter in the *grep* buffer
+(setq grep-find-ignored-files '(".#*" "*~"))
