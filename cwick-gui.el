72d75b45883c842a5ecde578f736b6b0dd06dc47
Carmen Wick <CarmenWick@gmail.com>
2011-06-03 02:34:39 -0700

    Remove menu bars

--- cwick-gui.el
+++ cwick-gui.el
@@ -1,6 +1,8 @@
 ;;;; Set GUI options
 ;; toolbar? we don't need no stinkin' toolbar
 (tool-bar-mode -1)
+(menu-bar-mode -1)                      ; Ditto for menus
+
 ;; Display full path to file in title bar
 (setq frame-title-format
       '(buffer-file-name "%f" (dired-directory dired-directory "%b")))
