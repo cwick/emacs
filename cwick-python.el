e07ebdc7f3ab5feebeb399575aa14941f9c0647d
Carmen Wick <CarmenWick@gmail.com>
2011-09-30 13:44:31 -0700

    Add Pymacs

--- cwick-python.el
+++ cwick-python.el
@@ -71,6 +71,16 @@
 		ad-do-it))))
 
 
-
-
+; Activate Python ident advice
 (ad-activate 'python-calculate-indentation)
+
+;;
+;; Enable Pymacs
+;;
+(autoload 'pymacs-apply "pymacs")
+(autoload 'pymacs-call "pymacs")
+(autoload 'pymacs-eval "pymacs" nil t)
+(autoload 'pymacs-exec "pymacs" nil t)
+(autoload 'pymacs-load "pymacs" nil t)
+;;(eval-after-load "pymacs"
+;;  '(add-to-list 'pymacs-load-path YOUR-PYMACS-DIRECTORY"))
