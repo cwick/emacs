0af75c966db742807654647efde501835be8125e
Carmen Wick <CarmenWick@gmail.com>
2011-11-18 09:32:38 -0800

    Mumamo for rails

--- cwick-mumamo.el
+++ cwick-mumamo.el
@@ -1,9 +1,12 @@
 ;; Multi major mode
 (load "~/.emacs.d/vendor/nxhtml/util/mumamo-fun.el")
 
-; Enable mumamo whenever we load an html file
+;; Enable mumamo automatically
 (add-to-list 'auto-mode-alist '("\\.html\\'" . html-mumamo-mode))
-; Need to set this otherwise Mumamo does not pick the correct indent function
+(add-to-list 'auto-mode-alist '("\\.rhtml\\'" . eruby-html-mumamo))
+(add-to-list 'auto-mode-alist '("\\.html\\.erb\\'" . eruby-html-mumamo))
+
+;; Need to set this otherwise Mumamo does not pick the correct indent function
 (setq mumamo-indent-major-to-use '((html-mode html-mode)))
-; Only color first submode
+;; Only color first submode
 (setq mumamo-chunk-coloring 1)
