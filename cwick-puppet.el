033a845b83e0975a949a5b71d0a92aad450169e3
Carmen Wick <CarmenWick@gmail.com>
2011-12-18 21:21:28 -0800

    Add electric pairs to Puppet

--- cwick-puppet.el
+++ cwick-puppet.el
@@ -4,3 +4,10 @@
 (autoload 'puppet-mode "puppet-mode" "Major mode for editing puppet manifests")
 
 (add-to-list 'auto-mode-alist '("\\.pp$" . puppet-mode))
+
+
+(add-hook 'puppet-mode-hook
+          (lambda ()
+            (progn
+              ;; Turn on electric pairs
+              (setup-electric-pairs puppet-mode-map))))
