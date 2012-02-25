560d294a403c60ae0f516ddaefd14f98f12fb5f5
Carmen Wick <CarmenWick@gmail.com>
2012-02-24 23:54:52 -0800

    feature-mode. Fix ruby-mode syntax highlighting

--- init.el
+++ init.el
@@ -120,6 +120,9 @@
 ;; YAML
 (load-library "cwick-yaml")
 
+;; Cucumber
+(load-library "cwick-feature-mode")
+
 ;; OSX-specific stuff
 (if (eq system-type 'darwin)
 	(load-library "cwick-osx"))
@@ -183,7 +186,6 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(menu-bar-mode nil)
- '(show-trailing-whitespace t)
  '(tab-stop-list (quote (4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80 84 88 92 96 100 104 108 112 116 120))))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
