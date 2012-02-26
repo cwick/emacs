7cfaab117d3fcd9ba5c31a6ca9985573c694184c
Carmen Wick <CarmenWick@gmail.com>
2012-02-25 19:53:23 -0800

    Add Rakefile support. Update RVM version

--- cwick-feature-mode.el
+++ cwick-feature-mode.el
@@ -1,2 +1,3 @@
 (require 'feature-mode)
 (add-to-list 'auto-mode-alist '("\.feature$" . feature-mode))
+(setq feature-cucumber-command "cucumber {feature}")
--- cwick-ruby.el
+++ cwick-ruby.el
@@ -1,8 +1,9 @@
 (require 'rvm)
-(rvm-use-default) ;; use rvm's default ruby for the current Emacs session
+(rvm-use-default)
 
 (add-to-list 'auto-mode-alist '("\\.rb\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\Rakefile\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\Gemfile\\'" . ruby-mode))
 
 (add-hook 'ruby-mode-hook
